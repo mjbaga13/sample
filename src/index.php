84c88f79da86304392ca48b2a46466d4fbb9319c
Marvin <marvin.baga@oneshift.com>
2016-09-29 17:55:18 +0800

    I want to share this commit

--- src/index.php
+++ src/index.php
@@ -27,6 +27,7 @@ $now = Carbon::now();
     	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos facere officia consequatur provident at explicabo libero voluptate quam, tempora harum.</p>
     	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident amet possimus dolorum laboriosam minus sunt earum, iure suscipit officia, obcaecati, doloribus quod nobis! Vel accusamus facilis cumque animi, saepe labore laudantium explicabo voluptate quia aliquam incidunt quidem quis voluptatem voluptates illum aperiam, ipsa? Porro maiores cupiditate officia cum, tempora deserunt.</p>
     	<p>Change from another developer.</p>
+    	<p>Change I want you to see.</p>
 	</div>
 </body>
 </html>
